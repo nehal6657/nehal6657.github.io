76c92cf70e7bd9c21dfff1dced3e91498d9cbeba
nehal6657 <54617734+nehal6657@users.noreply.github.com>
2020-07-03 20:34:11 +0530

    footer added

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//sticky nav menu
+
+$(document).ready(function(){
+
+    let nav_offset_top=$('.header_area').height()+50;
+    function navbarFixed(){
+        if($('.header_area').length()){
+            $(window).scroll(function(){
+                let scroll=$(window).scrollTop();
+                if(scroll>=nav_offset_top){ 
+                    $('.header_area .main_menu').addClass("navbar_fixed");
+                }
+                else{
+                    $('.header_area .main_menu').removeClass("navbar_fixed");
+                }
+            })
+        }
+    }
+    navbarFixed();
+
+});
